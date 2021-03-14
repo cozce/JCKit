fb38a318ba0fa5dd5ec7b09032fe1f157222fdcb
Jeff Chavez <jeffchavez8588@gmail.com>
2021-03-13 19:45:47 -0800

    Initial Commit

---
+++ JCKit/JCKit.h
@@ -0,0 +1,18 @@
+//
+//  JCKit.h
+//  JCKit
+//
+//  Created by Jeff Chavez on 3/13/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JCKit.
+FOUNDATION_EXPORT double JCKitVersionNumber;
+
+//! Project version string for JCKit.
+FOUNDATION_EXPORT const unsigned char JCKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JCKit/PublicHeader.h>
+
+
